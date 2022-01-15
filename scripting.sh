acde940ede0ec901d3edf455c7f54a77c4602c05
Rimun <rimunarmstrong@yahoo.com>
2022-01-15 08:08:12 -0500

    cours

---
+++ scripting.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "What is your name?"
+read na
+echo "Where were you born?"
+read y
+echo "Why are you here?"
+read r
+echo "Your name is ${na}, you were born in ${y}, and are here for ${r}.
